314940606429568e8f48ec83fd148e54f8d6a7c7
RJ Taylor <70663575+rjtayl@users.noreply.github.com>
2022-11-11 10:25:51 -0500

    Delete src directory

--- src/plotting.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
-import matplotlib.animation as animation
-
-plt.rcParams['figure.figsize'] = [8, 5]
-plt.rcParams.update({'font.size': 18})

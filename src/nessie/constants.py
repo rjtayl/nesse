be7a8c0b0b40731fa1ddff343b72f448ac7dd6bf
RJ Taylor <70663575+rjtayl@users.noreply.github.com>
2022-11-11 10:26:09 -0500

    Add files via upload

---
+++ src/nessie/constants.py
@@ -0,0 +1,6 @@
+me_SI = 9e-31 # electron mass in kg
+qe_SI = 1.6e-19 # electron charge in C
+kB_SI = 1.38e-23 # Boltzmann constant in SI units
+kB = 8.617e-5 # boltzmann constant in eV/K
+kE = 8.99e9 # 1/(4 pi epsilon0) in SI units
+epsR = 11.8 # dielectric strength of Silicon

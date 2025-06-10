40ffeb8674e0d0ddafab61b008e1a30f55f337f5
rjtayl <rtaylor19@live.com>
2025-06-10 11:58:02 -0400

    some vectorization speed ups to interp_3d and removing old functions

--- src/nesse/interp_3d.py
+++ src/nesse/interp_3d.py
@@ -14,39 +14,17 @@ def find_first(item, vec):
     return -1
 
 @guvectorize([(float64, float64[:], int64[:])], '(),(n)->()', nopython=True)
-def find_first(item, vec, out):
+def find_first_vector(item, vec, out):
     out[0] = -1
     for i in range(vec.shape[0]):
         if item < vec[i]:
             out[0] = i
             break  
-    
-@jit(nopython=True)
-def bisect_left(a, x):
-    hi = len(a)
-    lo = 0
-    while lo < hi:
-        mid = (lo + hi) // 2
-        if a[mid] < x:
-            lo = mid + 1
-        else:
-            hi = mid
-    return lo
  
-@jit(nopython=True)
-def get_ijk(t, x, y, z):
-    i_s = [0,0,0]
-    axes = [x,y,z]
-    for d in range(3):
-        for i, val in enumerate(axes[d]):
-            if t[d] < val:
-                i_s[d] = i -1
-                
-    return i_s
 
 @guvectorize([(float64[:], float64[:], float64[:], float64[:], int64[:])], 
-             '(d),(n),(n),(n)->(d)', nopython=True)
-def get_ijk(t, x, y, z, out):
+             '(d),(l),(m),(n)->(d)', nopython=True)
+def get_ijk_vector(t, x, y, z, out):
     axes = [x, y, z]
     for d in range(3):
         out[d] = 0
@@ -56,7 +34,7 @@ def get_ijk(t, x, y, z, out):
                 break
     
 
-class Interp3D(object):
+class Interp3D_old(object):
     '''
     Grid interpolator for 3-dimensional regular rectangular grid. 
     This is an old version, see function below.
@@ -98,44 +76,17 @@ class Interp3D(object):
         
     def get_ijk(self, t):
         return find_first(t[0], self.x)-1, find_first(t[1], self.y)-1, find_first(t[2], self.z)-1
-
-    def get_lmn(self, t, i, j, k):
-        return i + (t[0]-self.x[i])/self.delta_x[i], j + (t[1]-self.y[j])/self.delta_y[j], k + (t[2]-self.z[k])/self.delta_z[k]
-
-    def __call__(self, t):
-        X,Y,Z = self.v.shape[0], self.v.shape[1], self.v.shape[2]
-
-        #i = np.where(self.x>t[0])[0][0]-1
-        #j = np.where(self.y>t[1])[0][0]-1
-        #k = np.where(self.z>t[2])[0][0]-1
-
-        i,j,k = self.get_ijk(t)
-        
-        l,m,n = self.get_lmn(t, i, j, k)
-
-        return _interp3D(self.v, l, m, n, X, Y, Z)
     
-
-class Interp3D(object):
-    '''
-    Grid interpolator for 3-dimensional rectangular grid. 
-    Note that this is changed from above to work for non-regular grids.
-    get_ijk and get_lmn are seperate functions mainly for testing purposes.
-    '''
-    def __init__(self, v, x, y, z):
-        self.v = v
-        self.x = x
-        self.y = y
-        self.z = z
-        self.delta_x = np.diff(x)
-        self.delta_y = np.diff(y)
-        self.delta_z = np.diff(z)
-        
-    def get_ijk(self, t):
-        return find_first(t[0], self.x)-1, find_first(t[1], self.y)-1, find_first(t[2], self.z)-1
-
     def get_lmn(self, t, i, j, k):
         return i + (t[0]-self.x[i])/self.delta_x[i], j + (t[1]-self.y[j])/self.delta_y[j], k + (t[2]-self.z[k])/self.delta_z[k]
+    
+    def get_lmn_vector(self, t, i, j, k):
+        #tried converting to jit, didn't speed up anything. 
+        # Vectorized version: t is (N, 3), i, j, k are arrays of length N
+        l = i + (t[:, 0] - self.x[i]) / self.delta_x[i]
+        m = j + (t[:, 1] - self.y[j]) / self.delta_y[j]
+        n = k + (t[:, 2] - self.z[k]) / self.delta_z[k]
+        return l, m, n
 
     def __call__(self, t):
         # Allow t to be a single coordinate (shape (3,)) or an array of coordinates (shape (N, 3))
@@ -149,11 +100,18 @@ class Interp3D(object):
         elif t.ndim == 2 and t.shape[1] == 3:
             # Array of coordinates
             X, Y, Z = self.v.shape
+
+            ijks= get_ijk_vector(t, self.x, self.y, self.z)
+            i = ijks[:, 0]
+            j = ijks[:, 1]
+            k = ijks[:, 2]
+
+            l, m, n = self.get_lmn_vector(t, i, j, k)
+
+            #TODO: vectorize _interp3D
             results = np.empty(t.shape[0], dtype=self.v.dtype)
             for idx, coord in enumerate(t):
-                i, j, k = self.get_ijk(coord)
-                l, m, n = self.get_lmn(coord, i, j, k)
-                results[idx] = _interp3D(self.v, l, m, n, X, Y, Z)
+                results[idx] = _interp3D(self.v, l[idx], m[idx], n[idx], X, Y, Z)
             return results
         else:
             raise ValueError("Input t must be shape (3,) or (N, 3)")
--- tests/interp_test.py
+++ tests/interp_test.py
@@ -8,24 +8,52 @@ import matplotlib.pyplot as plt
 import cProfile
 import pstats
 from pstats import SortKey
+import time
 
 #@profile
 def main():
     #import SSD fields
-    EF_filename = "config/Fields/NessieEF_4e7Linear0-150V_grid.hf"
+    EF_filename = "config/Fields/NessieEF_Base4e7Linear0-150.0V.hf"
 
-    Efield=nesse.eFieldFromH5(EF_filename)  
+    Efield=nesse.fieldFromH5(EF_filename, rotate90=True)  
 
     eFieldx_interp, eFieldy_interp, eFieldz_interp, eFieldMag_interp = Efield.interpolate(True)
     
     #Ef = [eFieldz_interp([0,0,z]) for z in np.linspace(-0.001,0.002,10000)]
     
     zs = list(np.linspace(-0.001,0.002,100000))
+    coords = [[0,0,z] for z in zs]
     cProfile.runctx('[eFieldz_interp([0,0,z]) for z in zs]', {'eFieldz_interp':eFieldz_interp, 'zs':zs},{}, 'interp_stats')
+
+    p1 = pstats.Stats('interp_stats')
+    p1.sort_stats(SortKey.FILENAME).print_stats('interp_3d.py')
+    p1.sort_stats(SortKey.FILENAME).print_stats('_interp3D')
     
-    p = pstats.Stats('interp_stats')
+    #Note: cProfile doesnt appear to play nice with jit functions so its reporting a much longer run time than real
+    cProfile.runctx('eFieldz_interp(coords)', {'eFieldz_interp':eFieldz_interp, 'coords':coords},{}, 'interp_stats_vector')
+    
+    p = pstats.Stats('interp_stats_vector')
     p.sort_stats(SortKey.FILENAME).print_stats('interp_3d.py')
     p.sort_stats(SortKey.FILENAME).print_stats('_interp3D')
+
+    t0 = time.time()
+    efields_old = [eFieldz_interp([0,0,z]) for z in zs]
+    t1 = time.time()
+    efields_new = eFieldz_interp(coords)
+    t2 = time.time()
+
+    print(f"Old time: {t1-t0} \n New time: {t2-t1}")
+
+    diff = efields_new-efields_old
+    print(np.average(diff))
+    print(diff[111:130])
+    print(efields_new[111:130])
+    print(efields_old[111:130])
+    # print(np.argwhere(Efield.grid[0]==0.0))
+    # print(np.argwhere(Efield.grid[1]==0.0))
+    # print(np.argwhere(Efield.grid[2]==0.0))
+    # print(np.argwhere(Efield.grid[2]==0.002))
+    # print(Efield.fieldz[187,172,20])
 
 if __name__ == "__main__":
     #cProfile.run('main()')

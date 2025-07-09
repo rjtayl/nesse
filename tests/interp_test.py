b8e8f7dd2d60518a1f8a5cdb7ac41b222321029d
rjtayl <rtaylor19@live.com>
2025-07-09 17:48:32 -0400

    interp test vectorization

--- tests/interp_test.py
+++ tests/interp_test.py
@@ -40,5 +40,15 @@ def test_interp():
 
     assert expected == interped, f"Interpolation returns non-zero result out of bounds: {interped}"
 
+    #check vectorization gets same results
+
+    positions = np.array([[0,0,0], [0.5,0.5,1], [0.5,0.5,0.15], [-1.05,0,0],[-1,-1,-2], [1,1,2], [2,0,0]], dtype=np.float32)
+
+    interped = np.array([np.round(interp(pos), 5) for pos in positions],dtype=np.float32)
+
+    interped_vector =np.array([np.round(val, 5) for val in interp(positions)],dtype=np.float32)
+
+    assert np.all(interped_vector == interped), f"Vectorized interpolation not working: {interped}, {interped_vector}"
+
 if __name__ == "__main__":
     test_interp()  

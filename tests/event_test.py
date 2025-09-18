4c751858e6409f4f7a95d64fdfbd686968825c50
rjtayl <rtaylor19@live.com>
2025-09-18 12:56:21 -0400

    added test for nabsim event input

--- tests/event_test.py
+++ tests/event_test.py
@@ -21,7 +21,7 @@ def test_event():
 
     assert np.array_equal(saved_events[0].dE, events[0].dE), f"Monoenergetic electron file energies imported incorrectly."
 
-    #test "new" monoenergetic proton files
+    #test "new" monoenergetic electron files
     filename = "./config/Events/109Cd_Setup_e-_62.5keV.root"
     events = nesse.eventsFromG4root(filename, N=10)
     saved_events = nesse.loadEvents("./tests/62.5_109Cd_events")
@@ -33,6 +33,25 @@ def test_event():
     assert np.array_equal(saved_events[0].times, events[0].times), f"Cd electron file times imported incorrectly."
 
     assert np.array_equal(saved_events[0].dE, events[0].dE), f"Cd electron file energies imported incorrectly."
+
+    #test "NabSim" event files
+    filename = "./config/Events/1kEvents_eventNum_0.root"
+    events = nesse.eventsFromG4root(filename, N=10, nab_file=True)
+    saved_events = nesse.loadEvents("./tests/1kEvents_eventNum_0")
+
+    assert list(map(lambda x: x.ID, events[:3])) == list(map(lambda x: x.ID, saved_events)), f"Nabsim file event IDs imported incorrectly, expected: {list(map(lambda x: x.ID, saved_events[:3]))}, found: {list(map(lambda x: x.ID, events[:3]))}"
+
+    assert np.array_equal(saved_events[0].pos, events[0].pos), f"Nabsim electron positions imported incorrectly."
+
+    assert np.array_equal(saved_events[0].times, events[0].times), f"Nabsim electron times imported incorrectly."
+
+    assert np.array_equal(saved_events[0].dE, events[0].dE), f"Nabsim electron energies imported incorrectly."
+
+    assert np.array_equal(saved_events[1].pos, events[1].pos), f"Nabsim proton positions imported incorrectly."
+
+    assert np.array_equal(saved_events[1].times, events[1].times), f"Nabsim proton times imported incorrectly."
+
+    assert np.array_equal(saved_events[1].dE, events[1].dE), f"Nabsim proton energies imported incorrectly."
 
 
 
